f0961170a9c59ab9278bfacb6fac0f06d0df84a6
LostoVayne <moises.barrera.dev@gmail.com>
2025-10-06 01:52:27 -0300

    Update RabbitMQ configuration and add Docker update script

--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RmqService } from './rmq.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [RmqService],
   exports: [RmqService],
 })
